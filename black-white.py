7d4efbd8c08199e2de8b4b746d2cd0c904f3c486
superfroggman <49985901+superfroggman@users.noreply.github.com>
2019-05-22 11:20:14 +0200

    Complete rewrite of script

--- black-white.py
+++ black-white.py
@@ -1,21 +1,16 @@
-import pygame
 import sys
+from PIL import Image
+import numpy as np
 
-pygame.init()
- 
-print (sys.argv[1])
-# my stuff
-image = pygame.image.load(sys.argv[1])
+img = Image.open(sys.argv[1])
+rgb_im = img.convert('RGB')
+pixels = img.load() # Create the pixel map
+for i in range(img.size[0]):    # For every pixel:
+    for j in range(img.size[1]):
+        r, g, b = rgb_im.getpixel((i, j))
+        total = int((r+g+b)/3)
+        pixels[i,j] = (total,total,total)
 
-for x in xrange(0, image.get_rect().size[0]):
-    print (x+1)
-    print ("/")
-    print (image.get_rect().size[0])
-    print ("-----------")
-    for y in xrange(0, image.get_rect().size[1]):
-        pixel = image.get_at((x, y))
-        color = (pixel[0]+pixel[1]+pixel[2])/3
-        image.set_at((x,y), (color,color,color))
+img.show()
+img.save(sys.argv[2])
 print ("DONE!")
-
-pygame.image.save(image, sys.argv[2])
